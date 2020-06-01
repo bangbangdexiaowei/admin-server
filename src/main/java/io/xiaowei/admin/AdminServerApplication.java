720ba1d33e312a56c6a4e57d433bd0f83fa626a9
xiaowei <xiaowei_wang@aliyun.com>
2020-06-02 00:15:29 +0800

    init

---
+++ src/main/java/io/xiaowei/admin/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package io.xiaowei.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@Slf4j
+@EnableEurekaClient
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
